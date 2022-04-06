bee0979e57f78f89765c52bae019b66457acaea6
Carlo de Franchis <carlodef@gmail.com>
2022-04-06 13:31:03 +0200

    add sigmoid in inference script

--- infer_oscd.py
+++ infer_oscd.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import numpy as np
+import torch
 from torchvision.transforms import functional as TF
 
 from datasets import oscd_dataset
@@ -17,7 +18,8 @@ im2 = oscd_dataset.read_image(pathlib.Path("datasets/oscd/abudhabi/imgs_2"), ban
 # c2 = im2.crop((0, 0, 96, 96))
 # x1 = TF.to_tensor(c1).unsqueeze(0)
 # x2 = TF.to_tensor(c2).unsqueeze(0)
-# kk = model.forward(x1, x2)
+# out = model(x1, x2)
+# pred = torch.sigmoid(out)
 
 output = np.zeros((im1.height, im1.width))
 
@@ -25,6 +27,7 @@ for x in range(0, im1.width - 96, 96):
     for y in range(0, im1.height - 96, 96):
         c1 = im1.crop((x, y, x+96, y+96))
         c2 = im2.crop((x, y, x+96, y+96))
-        kk = model.forward(TF.to_tensor(c1).unsqueeze(0),
-                           TF.to_tensor(c2).unsqueeze(0))
-        output[y:y+96, x:x+96] = kk.detach().numpy().squeeze()
+        out = model(TF.to_tensor(c1).unsqueeze(0),
+                    TF.to_tensor(c2).unsqueeze(0))
+        pred = torch.sigmoid(out)
+        output[y:y+96, x:x+96] = pred.detach().numpy().squeeze()
